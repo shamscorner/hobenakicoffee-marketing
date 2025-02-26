cb118e6b8eef7e19e1c06dd3ca1689dd2c0da010
Shamim <hossains159@gmail.com>
2025-02-26 06:09:11 +0600

    feat: add SITE environment variable and update Google Tag Manager script

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
 	readonly PUBLIC_SIGN_UP_PAGE_URL: string;
 	readonly PUBLIC_SIGN_IN_PAGE_URL: string;
 	readonly GTM_ID: string;
+	readonly SITE: string;
 }
 
 interface ImportMeta {
