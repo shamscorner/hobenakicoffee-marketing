0a68c9b65a4565645f7f184c1940b34a603ee44e
Shamim Hossain <hossains159@gmail.com>
2025-02-27 04:56:55 +0600

    Staging (#2)
    * feat: add Clarity integration and corresponding environment variable
    
    * fix: remove Clarity integration from analytics component
    
    * feat: integrate Unleash feature flagging and update environment configuration

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,7 +5,9 @@ interface ImportMetaEnv {
 	readonly PUBLIC_SIGN_UP_PAGE_URL: string;
 	readonly PUBLIC_SIGN_IN_PAGE_URL: string;
 	readonly GTM_ID: string;
+	readonly CLARITY_ID: string;
 	readonly SITE: string;
+	readonly UNLEASH_API_KEY: string;
 }
 
 interface ImportMeta {
