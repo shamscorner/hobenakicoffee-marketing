dcc712bb18909dfcb05617a2e4433ba3d70637cd
Shamim <hossains159@gmail.com>
2025-02-26 06:49:39 +0600

    fix: faq item selection area issue

--- src/components/faqs/faqs.svelte
+++ src/components/faqs/faqs.svelte
@@ -6,9 +6,16 @@
 
 <Accordion.Root type="single" class="w-full">
 	{#each faqs as faq, index}
-		<Accordion.Item value={`item-${index}`}>
-			<Accordion.Trigger>{faq.question}</Accordion.Trigger>
-			<Accordion.Content>{faq.answer}</Accordion.Content>
+		<Accordion.Item
+			value={`item-${index}`}
+			class="my-4 rounded-xl border-2 px-4 transition hover:border-primary"
+		>
+			<Accordion.Trigger
+				class="py-6 text-base font-semibold transition hover:text-primary hover:no-underline"
+			>
+				{faq.question}
+			</Accordion.Trigger>
+			<Accordion.Content class="text-base">{faq.answer}</Accordion.Content>
 		</Accordion.Item>
 	{/each}
 </Accordion.Root>
--- src/components/ui/accordion/accordion-item.svelte
+++ src/components/ui/accordion/accordion-item.svelte
@@ -2,20 +2,11 @@
 	import { Accordion as AccordionPrimitive } from 'bits-ui';
 	import { cn } from '$lib/utils.js';
 
-	type $$Props = AccordionPrimitive.ItemProps;
-
-	let className: $$Props['class'] = undefined;
-	export { className as class };
-	export let value: $$Props['value'];
+	let {
+		ref = $bindable(null),
+		class: className,
+		...restProps
+	}: AccordionPrimitive.ItemProps = $props();
 </script>
 
-<AccordionPrimitive.Item
-	{value}
-	class={cn(
-		'mb-6 overflow-hidden rounded-2xl border-2 border-foreground/40 px-4 py-4 transition duration-300 hover:border-foreground sm:px-6',
-		className
-	)}
-	{...$$restProps}
->
-	<slot />
-</AccordionPrimitive.Item>
+<AccordionPrimitive.Item bind:ref class={cn('border-b', className)} {...restProps} />
