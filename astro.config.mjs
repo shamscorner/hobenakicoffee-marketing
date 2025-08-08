22f8cd2112916d6f1564c18bf8c9efcf10f31a1b
Shamim <hossains159@gmail.com>
2025-08-09 03:34:40 +0600

    Update Bengali translations for product slogan and social media references
    - Revised the slogan to emphasize ease and safety in earning from fans.
    - Adjusted the social media profile description for clarity and consistency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@
 import { defineConfig } from 'astro/config';
 import { loadEnv } from 'vite';
 
-import node from '@astrojs/node';
-
 import sitemap from '@astrojs/sitemap';
 
 import robotsTxt from 'astro-robots-txt';
@@ -14,35 +12,35 @@ import svelte from '@astrojs/svelte';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 const { SITE: site } = loadEnv(process.env.NODE_ENV || '', process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
-	// site: 'https://hobenakicoffee.com',
-	site,
+    // site: 'https://hobenakicoffee.com',
+    site,
 
-	integrations: [
-		sitemap({
-			i18n: {
-				defaultLocale: 'bn',
-				locales: {
-					en: 'en-US',
-					bn: 'bn-BD',
-				},
-			},
-		}),
-		robotsTxt(),
-		partytown(),
-		svelte(),
-	],
+    integrations: [
+        sitemap({
+            i18n: {
+                defaultLocale: 'bn',
+                locales: {
+                    en: 'en-US',
+                    bn: 'bn-BD',
+                },
+            },
+        }),
+        robotsTxt(),
+        partytown(),
+        svelte(),
+    ],
 
-	output: 'server',
+    output: 'server',
 
-	adapter: node({
-		mode: 'standalone',
-	}),
+    adapter: vercel(),
 
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
